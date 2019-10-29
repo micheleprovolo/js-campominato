2371f1d30af860532c5e13fa9e78aaac39782a3d
michele <michele.provolo@gmail.com>
2019-10-29 23:25:10 +0100

    impostata funzione per controllare se numero è stato già inserito

--- main.js
+++ main.js
@@ -8,13 +8,38 @@
 // i 16 numeri vietati/mina, devono essere tutti diversi, non possono esserci doppioni;
 // l’utente non può inserire due volte lo stesso numero, ma sempre numeri diversi.
 
+//aray vuoto da popolare con numeri casuali mai doppi
+var userNumbers = []
 
-for (i = 0; i < 16; i++) {
+//var i = 0;
+//ciclo che genera 16 numeri da 1 a 100
+while (userNumbers.length  < 16) {
         var cpuNum = getRandomIntInclusive (1,100);
-        console.log(cpuNum);
+        var found = findInArray (cpuNum, userNumbers); //controllo se il numero random è già stato inserito nell'array
+        if (found == false) { //se non c'è
+                userNumbers.push(cpuNum); //viene inserito
+        }
+        console.log(found);
+        console.log("lunghezza array", userNumbers.length);
 }
 
+console.log(userNumbers);
+
+
 //BLOCCO FUNZIONI
+//funzione per controllare un valore dentro un array: restituisce true o false
+function findInArray (value, array) {
+        var found = false;
+        var i = 0;
+        while (found == false && i < array.length) {
+                if (array[i] == value) {
+                        found = true;
+                }
+                i++
+        }
+          return found;      
+}
+
 
 //funzione che genera un numero casuale compreso fra un min e max che imposto come argomento quando la invoco
 function getRandomIntInclusive(min, max) {
