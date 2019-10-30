6ee985890829053476154b3dd6813f4942ca27d3
michele <michele.provolo@gmail.com>
2019-10-30 23:49:33 +0100

    revisione ex

--- main.js
+++ main.js
@@ -8,23 +8,56 @@
 // i 16 numeri vietati/mina, devono essere tutti diversi, non possono esserci doppioni;
 // l’utente non può inserire due volte lo stesso numero, ma sempre numeri diversi.
 
-//aray vuoto da popolare con numeri casuali mai doppi
-var userNumbers = []
 
-//var i = 0;
+//array vuoto da popolare con numeri casuali mai doppi
+var mine = [];
+
 //ciclo che genera 16 numeri da 1 a 100
-while (userNumbers.length  < 16) {
+while (mine.length  < 16) {
         var cpuNum = getRandomIntInclusive (1,100);
-        var found = findInArray (cpuNum, userNumbers); //controllo se il numero random è già stato inserito nell'array
+        var found = findInArray (cpuNum, mine); //controllo se il numero random è già stato inserito nell'array
         if (found == false) { //se non c'è
-                userNumbers.push(cpuNum); //viene inserito
+                mine.push(cpuNum); //viene inserito
         }
-        console.log(found);
-        console.log("lunghezza array", userNumbers.length);
+        console.log(cpuNum);
+        
 }
 
-console.log(userNumbers);
+console.log("I numeri vietati sono: " + mine);
 
+
+//l'utente deve inserire un numero da 1 a 100
+var userNumbers = [];
+
+//finchè l'utente non gioca, il suo numero non sarà riscontrato tra quelli vieti e dunque sarà valorizzato a false
+var trovato = false;
+
+//il punteggio dell'utente parte da zero
+var score = 0;
+
+while (score < 84 && trovato == false) {
+
+        var askUser = parseInt(prompt("Inserisci un numero da 1 a 100"));
+        console.log("L'utente ha scelto " + askUser);
+
+if (askUser > 0 && askUser < 101) {
+        if (userNumbers.includes(askUser) == true){
+                alert("occhio! hai già inserito questo numero!")
+        } else if (mine.includes(askUser) == true){
+                alert("Hai pestato la mina: game over");
+                trovato = true;
+        } else {
+                alert("Hai fatto punto! Prosegui")
+                userNumbers.push(askUser);
+                score ++;
+        }
+} else {
+        alert("Inserisci numero valido cioè da 0 a 100");
+}
+}
+
+        
+       
 
 //BLOCCO FUNZIONI
 //funzione per controllare un valore dentro un array: restituisce true o false
@@ -47,4 +80,3 @@ function getRandomIntInclusive(min, max) {
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
